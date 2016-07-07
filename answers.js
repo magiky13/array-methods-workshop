d352e2a248bf842c25371573aa5874152ac5a61a
sisi <simdorvil@hotmail.com>
2016-07-07 18:28:10 +0000

    e1

--- answers.js
+++ answers.js
@@ -6,23 +6,16 @@ var getPositives = testArray.filter(function(numbers){
     return numbers > 0
 });
 
-console.log(getPositives)
+//console.log(getPositives)
 
 //e1
 
 var testArray = [1, 2, -3, -5];
 
-testArray.forEach(function(number){
+var printPositive = testArray.forEach(function(number){
     if(number > 0){
         console.log(number);
     }
 });
 
 //e2b
-var testArray = [1, 2, -3, -5];
-
-testArray.forEach(function(number){
-    return testArray.filter(number > 0);
-        forEach(number);
-        console.log(number);
-});
