62dfd91daa55a99aadfd5d75879bb3ed36decf33
sisi <simdorvil@hotmail.com>
2016-07-07 19:29:03 +0000

    e3 done not 2b

--- answers.js
+++ answers.js
@@ -9,13 +9,36 @@ var getPositives = testArray.filter(function(numbers){
 //console.log(getPositives)
 
 //e1
-
+/*
 var testArray = [1, 2, -3, -5];
 
-var printPositive = testArray.forEach(function(number){
+//var printPositive = testArray.forEach(function(number){
     if(number > 0){
-        console.log(number);
+        //console.log(number);
     }
 });
 
-//e2b
+//e2B
+
+var printPositive = testArray(function(number){
+    return testArray.filter(number > 0).forEach
+});
+
+*/
+//e3
+
+var testArray2 = [0, 1, 5, ""];
+
+var filterArray = function(array, func) {return array.filter(func);};
+
+var truthy = function(x){
+    if(x){
+        return x;
+    }
+};
+
+//console.log(filterArray(testArray2, truthy));
+
+//e4
+
+
