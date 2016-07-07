ad7d2ea6581e08cd3bb0d6089a72f4abde0aa883
sisi <simdorvil@hotmail.com>
2016-07-07 18:26:46 +0000

    e1 done now, e2b not at all

--- answers.js
+++ answers.js
@@ -1,11 +1,28 @@
+//e2
+
+var testArray = [1, 2, -3, -5];
+
+var getPositives = testArray.filter(function(numbers){
+    return numbers > 0
+});
+
+console.log(getPositives)
+
 //e1
 
 var testArray = [1, 2, -3, -5];
 
-var printPositives = testArray.filter(function(numbers){
-    return numbers > 0
+testArray.forEach(function(number){
+    if(number > 0){
+        console.log(number);
+    }
 });
 
-console.log(printPositives)
+//e2b
+var testArray = [1, 2, -3, -5];
 
-//e2
+testArray.forEach(function(number){
+    return testArray.filter(number > 0);
+        forEach(number);
+        console.log(number);
+});
